43c186dbf328d59d946bf5825e95c32741503c0a
MK Kang <mkkang0116@gmail.com>
2018-06-21 22:40:19 -0700

    Fix build error

--- android/src/main/java/co/squaretwo/ironsource/RNIronSourcePackage.java
+++ android/src/main/java/co/squaretwo/ironsource/RNIronSourcePackage.java
@@ -23,11 +23,6 @@ public class RNIronSourcePackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
     }
