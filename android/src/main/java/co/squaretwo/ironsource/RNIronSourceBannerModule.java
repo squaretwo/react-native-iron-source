2ff5da74787cfd524465c161de6e220c1b482904
Dmitriy Kazlouski <d.kazlouski@wowmaking.net>
2019-06-06 13:30:13 +0300

    Improved compatibility with RN 0.58

--- android/src/main/java/co/squaretwo/ironsource/RNIronSourceBannerModule.java
+++ android/src/main/java/co/squaretwo/ironsource/RNIronSourceBannerModule.java
@@ -239,7 +239,7 @@ public class RNIronSourceBannerModule extends ReactContextBaseJavaModule impleme
                 bannerLayout.removeAllViews();
             }
         });
-        loadPromise.reject("E_LOAD", "Failed to load banner", null);
+        loadPromise.reject("E_LOAD", "Failed to load banner");
         sendEvent("ironSourceBannerDidFailToLoadWithError", Utility.formatIronSourceError(error));
     }
 
